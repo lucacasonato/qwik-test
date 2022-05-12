aa7edecd1e85948c511958b14e12de082726757d
Luca Casonato <hello@lcas.dev>
2022-05-13 01:52:47 +0200

    actual server render, with client interaction working

--- server.ts
+++ server.ts
@@ -1,7 +1,22 @@
 // run with ``
 
+import { serve } from "https://deno.land/std/http/server.ts";
+import { serveDir } from "https://deno.land/std/http/file_server.ts";
 import { Root } from "./out/root.js";
 import { renderToString } from "@builder.io/qwik/server";
 
-const res = await renderToString(Root);
-console.log(res.html);
+import importMap from "./import_map.json" assert { type: "json" };
+
+serve(async (req) => {
+  const url = new URL(req.url);
+  if (url.pathname === "/") {
+    let { html } = await renderToString(Root);
+    html = html.replace("</head>", `<script type="importmap">${JSON.stringify(importMap)}</script></head>`);
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+    });
+  }
+  return serveDir(req, { fsRoot: "./out" });
+});
